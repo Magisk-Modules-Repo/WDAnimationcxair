c1225477c47d3ef06d2423ccbf31f0a28a84cdeb
Michael Bekele <43565227+mikesew1320@users.noreply.github.com>
2020-01-13 00:14:59 -0900

    Updated

--- common/unity_install.sh
+++ common/unity_install.sh
@@ -2,8 +2,9 @@
 #Script written by Mikesew1320
 #Contact me on Telegram, GitHub, XDA or Hovatek using my username.
 #Script written in Jimma, Ethiopia
-#Copyright FridayJanuary 11, 2020
+#Copyright FridayJanuary 13, 2020
 
+MODE=`getprop ro.product.system.device`
 MODEL=`getprop ro.product.model`
 RELEASE=`getprop ro.build.version.release`
 BRAND=$(getprop ro.product.brand | tr '[:lower:]' '[:upper:]')
@@ -31,7 +32,7 @@ else
   ui_print " CUSTOM RECOVERY INSTALLATION DETECTED! "
 fi
 case $DEVICE in 
-  "H3713"|"pine"|"a5y17lte"|"aosp_harpia"|"lineage_harpia") ui_print " SUPPORTED DEVICE DETECTED! ";;
+  "H3713"|"pine"|"a5y17lte"|"jackpotlte"|"aosp_harpia"|"lineage_harpia") ui_print " SUPPORTED DEVICE DETECTED! ";;
   *) abort "  => '"$DEVICE"' is not supported!";;
 esac
 if [ $RELEASE == "7.0" ] || [ $DEVICE == "lineage_harpia" ] ||  [ $DEVICE == "aosp_harpia" ] || [ $DEVICE == "TECNO" ]; then
@@ -61,7 +62,7 @@ if $VKSEL; then
     cp_ch $TMPDIR/Bootanimations/bootanimation2.zip $UNITY/system/media/bootanimation.zip
     cp_ch $TMPDIR/Bootanimations/bootanimation2.zip $UNITY/system/media/shutanimation.zip
     else
-  ui_print "                 WATCHDOGS-v3 BOOTANIMATION                 "
+  ui_print "              LINUX BOOTING BIOS BOOTANIMATION               "
   ui_print "         ******************************************         "
   ui_print "                 Vol Up = Yes, Vol Down = No                 "
   ui_print " "
@@ -96,22 +97,32 @@ if $VKSEL; then
   mkdir -p $UNITY/system/media
   cp_ch $TMPDIR/Bootanimations/bootanimation6.zip $UNITY/system/media/bootanimation.zip
   cp_ch $TMPDIR/Bootanimations/bootanimation6.zip $UNITY/system/media/shutanimation.zip
-  if $DEVICE == "H3713" ; then
+  else
+  ui_print "                 BOOTLEGGERS V2 BOOT ANIMATION                  "
+  ui_print "         ******************************************         "
+  ui_print "                 Vol Up = Yes, Vol Down = No                 "
+  ui_print " "
+  fi
+  if $VKSEL; then
+  mkdir -p $UNITY/system/media
+  cp_ch $TMPDIR/Bootanimations/bootanimation7.zip $UNITY/system/media/bootanimation.zip
+  cp_ch $TMPDIR/Bootanimations/bootanimation7.zip $UNITY/system/media/shutanimation.zip
+  fi
+  if [ $DEVICE =="H3713" ] && [ $RELEASE == 7.0]; then
 ui_print " Installing bootanimation on TECNO Camon CX Air..."
+fi
  if $BRAND == "Motorola" ; then
  ui_print " Installing bootanimation on Motorola Moto G4 Play..."
+ fi
 fi
 fi
 fi
 fi
 fi
 fi
-fi
-fi
-fi
-if [ $RELEASE == "9" ] && [ $MODEL == "SM-A520F" ];  then
+if [ $RELEASE == "9" ] && [ $MODEL == "SM-A520F" ] || [ $DEVICE == "jackpotlte"];  then
 ui_print " "
-ui_print "Start installing boot animation on Samsung Galaxy A5 2017... "
+ui_print " Start installing boot animation on Samsung Galaxy A5 2017... "
   ui_print " "
   ui_print "                 WATCHDOGS-v1 BOOTANIMATION                 "
   ui_print "         ******************************************         "
@@ -129,14 +140,13 @@ if $VKSEL; then
     mkdir -p $UNITY/system/media
     cp_ch $TMPDIR/Bootanimations/bootanimation22.zip $UNITY/system/media/bootanimation.zip
     else
-  ui_print "                 WATCHDOGS-v3 BOOTANIMATION                 "
+  ui_print "              LINUX BOOTING BIOS BOOTANIMATION               "
   ui_print "         ******************************************         "
   ui_print "                 Vol Up = Yes, Vol Down = No                 "
   ui_print " "
   if $VKSEL; then                
     mkdir -p $UNITY/system/media
     cp_ch $TMPDIR/Bootanimations/bootanimation33.zip $UNITY/system/media/bootanimation.zip
-ui_print "Installing bootanimation on SAMSUNG GALAXY A5 2017..."
 ui_print "          RESURRECTION REMIX OREO-V1 BOOTANIMATION                 "
   ui_print "         ******************************************         "
   ui_print "                Vol Up = Yes, Vol Down = No                 "
@@ -163,9 +173,51 @@ ui_print "          RESURRECTION REMIX OREO-V1 BOOTANIMATION                 "
   mkdir -p $UNITY/system/media
   cp_ch $TMPDIR/Bootanimations/bootanimation6.zip $UNITY/system/media/bootanimation.zip
   cp_ch $TMPDIR/Bootanimations/bootanimation6.zip $UNITY/system/media/shutanimation.zip
+  else
+  ui_print "                LINUX BOOTING BIOS BOOTANIMATION              "
+  ui_print "         ******************************************         "
+  ui_print "                          Vol Up = Yes, Vol Down = No                 "
+  ui_print " "
+  if $VKSEL; then
+  mkdir -p $UNITY/system/media
+  cp_ch $TMPDIR/Bootanimations/bootanimation7.zip $UNITY/system/media/bootanimation.zip
+  cp_ch $TMPDIR/Bootanimations/bootanimation7.zip $UNITY/system/media/shutanimation.zip
+  ui_print "Installing bootanimation on SAMSUNG GALAXY A5 2017..."
   fi
   fi
   fi
+  fi
+  fi
+  fi
+  fi
+  fi
+  if [ $RELEASE == "10" ] && [ $MODE == "beryllium" ];  then
+  ui_print "          RESURRECTION REMIX OREO-V2 BOOTANIMATION                 "
+  ui_print "         ******************************************         "
+  ui_print "                Vol Up = Yes, Vol Down = No                 "
+  ui_print " "
+  if $VKSEL; then
+  mkdir -p $UNITY/system/media
+  cp_ch $TMPDIR/Bootanimations/bootanimationPC1.zip $UNITY/system/media/bootanimation.zip
+  cp_ch $TMPDIR/Bootanimations/bootanimationPC1.zip $UNITY/system/media/shutanimation.zip
+  else
+    ui_print "            RESURRECTION REMIX PIE BOOTANIMATION                 "
+  ui_print "         ******************************************         "
+  ui_print "                Vol Up = Yes, Vol Down = No                 "
+  ui_print " "
+  if $VKSEL; then
+  mkdir -p $UNITY/system/media
+  cp_ch $TMPDIR/Bootanimations/bootanimationPC2.zip $UNITY/system/media/bootanimation.zip
+  cp_ch $TMPDIR/Bootanimations/bootanimationPC2.zip $UNITY/system/media/shutanimation.zip
+  else
+  ui_print "             LINUX BOOTING BIOS BOOTANIMATION               "
+  ui_print "         ******************************************         "
+  ui_print "                 Vol Up = Yes, Vol Down = No                 "
+  ui_print " "
+    if $VKSEL; then
+  mkdir -p $UNITY/system/media
+  cp_ch $TMPDIR/Bootanimations/bootanimationPC3.zip $UNITY/system/media/bootanimation.zip
+  cp_ch $TMPDIR/Bootanimations/bootanimationPC3.zip $UNITY/system/media/shutanimation.zip
   fi
   fi
   fi
@@ -194,17 +246,26 @@ if $VKSEL; then
   if $VKSEL; then                
     mkdir -p $UNITY/system/media
     cp_ch $TMPDIR/Bootanimations/bootanimation33.zip $UNITY/system/product/media/bootanimation.zip
+    else
+  ui_print "                LINUX BOOTING BIOS BOOTANIMATION              "
+  ui_print "         ******************************************         "
+  ui_print "                          Vol Up = Yes, Vol Down = No                 "
+  ui_print " "
+  if $VKSEL; then
+  mkdir -p $UNITY/system/media
+  cp_ch $TMPDIR/Bootanimations/bootanimation7.zip $UNITY/system/media/bootanimation.zip
+  cp_ch $TMPDIR/Bootanimations/bootanimation7.zip $UNITY/system/media/shutanimation.zip
+  fi
 ui_print " Installing bootanimation on Xiaomi Mi A1..."
 fi
 fi
 fi
 fi
-  ui_print " "
   ui_print "             ANIMATIONS SUCCESSFULLY INSTALLED!             "  
   ui_print " "
   ui_print "          Created by Mikesew1320 @ Hovatek Forum           "
   ui_print " "
   ui_print "  Join my Telegram channel http://t.me/MGStoreEthiopia.....  "
   ui_print " "
-  ui_print "                    © January 11-2020 G.C                     "
+  ui_print "                    © January 13-2020 G.C                     "
   ui_print " "
